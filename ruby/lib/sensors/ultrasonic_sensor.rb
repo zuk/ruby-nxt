9c53159089cb10609eab38a036bd62eac1ae9d1e
gunark <gunark@a85beeab-be1c-0410-a5ed-88d17ee140f9>
2006-10-10 01:22:29 +0000

    fixed ultrasonic_comm.rb path
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-nxt/trunk@74 a85beeab-be1c-0410-a5ed-88d17ee140f9

--- ruby/lib/sensors/ultrasonic_sensor.rb
+++ ruby/lib/sensors/ultrasonic_sensor.rb
@@ -15,7 +15,7 @@
 # Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # 
 require File.dirname(File.expand_path(__FILE__))+'/sensor'
-require File.dirname(File.expand_path(__FILE__))+'/ultrasonic_comm'
+require File.dirname(File.expand_path(__FILE__))+'/../ultrasonic_comm'
 
 class UltrasonicSensor < Sensor
   
